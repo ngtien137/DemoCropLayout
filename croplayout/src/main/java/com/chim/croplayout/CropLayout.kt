7ddcbd4dc466f29d53a47dab5be50a6c0a3354b6
Nguyễn Văn Tiến <ngtien137@gmail.com>
2020-06-30 23:03:47 +0700

    init

--- croplayout/src/main/java/com/chim/croplayout/CropLayout.kt
+++ croplayout/src/main/java/com/chim/croplayout/CropLayout.kt
@@ -1,9 +1,13 @@
 package com.chim.croplayout
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.ViewConfiguration
 import android.widget.FrameLayout
+import kotlin.math.abs
 
 class CropLayout @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -15,9 +19,18 @@ class CropLayout @JvmOverloads constructor(
     }
 
     private val rectView = RectF()
-    private val paintGrid = Paint(Paint.ANTI_ALIAS_FLAG) 
+    private val rectBorder = RectF()
+    private val paintGrid = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.STROKE
+    }
     private val paintGridBackground = Paint(Paint.ANTI_ALIAS_FLAG)
     private val pathGrid = Path()
+
+    private var touchAction = Touch.NONE
+    private val pointDown = PointF()
+    private val touchSlop by lazy {
+        ViewConfiguration.get(context).scaledTouchSlop
+    }
 
     init {
         init(attrs)
@@ -39,12 +52,19 @@ class CropLayout @JvmOverloads constructor(
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
+        val borderGridSizeHalf = paintGrid.strokeWidth / 2f
         rectView.set(0f, 0f, w, h)
-        pathGrid.moveTo(rectView.left,rectView.top)
-        pathGrid.lineTo(rectView.right,rectView.top)
-        pathGrid.lineTo(rectView.right,rectView.bottom)
-        pathGrid.lineTo(rectView.left,rectView.bottom)
-        pathGrid.lineTo(rectView.left,rectView.top)
+        rectBorder.set(
+            rectView.left + borderGridSizeHalf,
+            rectView.top + borderGridSizeHalf,
+            rectView.right - borderGridSizeHalf,
+            rectView.bottom - borderGridSizeHalf
+        )
+        pathGrid.moveTo(rectBorder.left, rectBorder.top)
+        pathGrid.lineTo(rectBorder.right, rectBorder.top)
+        pathGrid.lineTo(rectBorder.right, rectBorder.bottom)
+        pathGrid.lineTo(rectBorder.left, rectBorder.bottom)
+        pathGrid.lineTo(rectBorder.left, rectBorder.top)
     }
 
     override fun dispatchDraw(canvas: Canvas?) {
@@ -54,7 +74,45 @@ class CropLayout @JvmOverloads constructor(
         }
     }
 
-    private fun drawCropLayout(canvas:Canvas){
-        canvas.drawPath(pathGrid,paintGrid)
+    private fun drawCropLayout(canvas: Canvas) {
+        canvas.drawPath(pathGrid, paintGrid)
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+        ev?.let {
+            when (ev.actionMasked) {
+                MotionEvent.ACTION_DOWN -> {
+                    pointDown.set(ev.x, ev.y)
+                    //eLog("Down")
+                    return false
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    if (touchAction == Touch.MOVING) {
+                        //eLog("Moving")
+                        return false
+                    } else {
+                        if (touchAction != Touch.SCALING && touchAction != Touch.MOVING) {
+                            if (abs(ev.x - pointDown.x) > touchSlop || abs(ev.y - pointDown.y) > touchSlop) {
+                                touchAction = Touch.MOVING
+                            }
+                        }
+                        return false
+                    }
+                }
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    //eLog("Up")
+                    touchAction = Touch.NONE
+                    return true
+                }
+                else -> {
+                    return false
+                }
+            }
+        }
+        return super.onInterceptTouchEvent(ev)
+    }
+
+    enum class Touch {
+        NONE, MOVING, SCALING
     }
 }
